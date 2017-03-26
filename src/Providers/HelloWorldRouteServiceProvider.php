d7a38964a4eb14fab2c239c75dbc9e3716551502
mcone2004 <info@greifswald-design.de>
2017-03-26 23:38:57 +0200

    free
    test

---
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace gdesign\Providers;
+
+
+use Plenty\Plugin\RouteServiceProvider;
+use Plenty\Plugin\Routing\Router;
+
+class HelloWorldRouteServiceProvider extends RouteServiceProvider
+{
+    public function map(Router $router)
+    {
+
+    }
+}
